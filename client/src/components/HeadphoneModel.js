ce521d6e5d1365305ef1297b5c7dcd9395a6502e
jeffreycao1998 <jeffreycao1998@hotmail.com>
2020-09-26 15:44:39 -0400

    finished rotating model

--- client/src/components/HeadphoneModel.js
+++ client/src/components/HeadphoneModel.js
@@ -1,39 +1,75 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { Section } from './Section';
-import { Canvas } from 'react-three-fiber';
+import { Canvas, useFrame } from 'react-three-fiber';
 import { Html, useGLTFLoader } from 'drei';
-import turntable from '../assets/turntable.gltf';
+import turntable from '../assets/turntable.glb';
+
+import state from '../state';
 
 const HeadphoneModelContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  
+  .scrollArea {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    overflow: auto;
+  }
 
+  .container {
+    margin: 0 auto;
+    width: 100%;
+    max-width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 `;
 
 const HTMLContentContainer = styled.div`
-  width: 600px;
-  height: 600px;
+  display: flex;
+  position: absolute;
 
   .title {
     color: white;
   }
 `;
 
-const Model = () => {
-  const gltf = useGLTFLoader('../assets/turntable.gltf', true);
+const Model = ({ modelPath }) => {
+  const gltf = useGLTFLoader(modelPath, true);
   return <primitive object={gltf.scene} dispose={null} />;
 };
 
-const HTMLContent = () => {
+const Lights = () => {
+  return (
+    <>
+      <ambientLight intensity={.3} />;
+      <directionalLight position={[10, 10, 5]} intensity={1} />;
+      <directionalLight position={[0, 10, 0]} intensity={1.5} />;
+      <spotLight position={[1000, 0, 0]} intensity={1} />
+    </>
+  );
+};
+
+const HTMLContent = ({ domContent, children, modelPath, positionY }) => {
+
+  const ref = useRef();
+  useFrame(() => (ref.current.rotation.y += 0.01));
+
+
   return(
     <Section factor={1.5} offset={1}>
-      <group position={[0, 250, 0]}>
-        <mesh position={[0, 30, 0]}>
-          <Model />
+      <group position={[0, positionY, 0]}>
+        <mesh ref={ref} position={[0, 30, 0]}>
+          <Model modelPath={modelPath}/>
         </mesh>
-        <Html fullscreen>
-          <HTMLContentContainer>
-            <h1 className='title'>Hello</h1>
-          </HTMLContentContainer>
+        <Html portal={domContent} fullscreen>
+          {children}
         </Html>
       </group>
     </Section>
@@ -41,13 +77,29 @@ const HTMLContent = () => {
 };
 
 export default function HeadphoneModel() {
+  const domContent = useRef();
+  const scrollArea = useRef();
+  const onScroll = (e) => (state.top.current = e.target.scrollTop);
+
+  useEffect(() => void onScroll({ target: scrollArea.current }),[]);
+
   return(
     <HeadphoneModelContainer>
-      <Canvas colorManagement camera={{ position: [0, 0, 120], fov: 70 }}>
+      <Canvas colorManagement camera={{ position: [0, 0, 115], fov: 85 }}>
+        <Lights />
         <Suspense fallback={null}>
-          <HTMLContent />
+          <HTMLContent 
+            domContent={domContent} 
+            modelPath={turntable} 
+            positionY={560} >
+            <HTMLContentContainer />
+          </HTMLContent>
         </Suspense>
       </Canvas>
+      <div className="scrollArea" ref={scrollArea} onScroll={onScroll}>
+        <div style={{position: 'sticky', top: 0}} ref={domContent}></div>
+        <div style={{height: `${state.sections * 100}vh`}}></div>
+      </div>
     </HeadphoneModelContainer>
   );
 };
--- client/src/state/index.js
+++ client/src/state/index.js
@@ -1,7 +1,7 @@
 import { createRef } from 'react';
 
 const state = {
-  sections: 3,
+  sections: 2,
   pages: 3,
   zoom: 1,
   top: createRef(),
