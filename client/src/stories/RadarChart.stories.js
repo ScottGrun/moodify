d871d06f2c2509069461a8b8ed205f1306e0524f
Scott Grunerud <sgrunerud@edu.uwaterloo.ca>
2020-09-27 15:27:38 -0400

    fixing merge error

--- client/src/stories/RadarChart.stories.js
+++ client/src/stories/RadarChart.stories.js
@@ -1,15 +1,15 @@
-import RadarChart from '../components/Main/RadarChart';
-import React, { useState, useEffect, useContext } from 'react';
-import { StateContext } from '../App';
-import styled from 'styled-components';
-import { Radar } from 'react-chartjs-2';
+// import RadarChart from '../components/Main/RadarChart';
+// import React, { useState, useEffect, useContext } from 'react';
+// import { StateContext } from '../App';
+// import styled from 'styled-components';
+// import { Radar } from 'react-chartjs-2';
 
-export default {
-  title: "RadarChart",
-  component: RadarChart,
-};
+// export default {
+//   title: "RadarChart",
+//   component: RadarChart,
+// };
 
-const RadarChartTemplate = (args) => <RadarChart {...args} />;
+// const RadarChartTemplate = (args) => <RadarChart {...args} />;
 
-export const RadarChart1 = RadarChartTemplate.bind({});
+// export const RadarChart1 = RadarChartTemplate.bind({});
 
