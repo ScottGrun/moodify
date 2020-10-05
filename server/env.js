d0ee36840444c80f91018f024dccc21cce9574c4
Tania Nardandrea-Isakovic <rtnrtn@gmail.com>
2020-10-05 05:56:06 +0000

    Fixed definitions of clientRoot and serverRoot.

--- server/env.js
+++ server/env.js
@@ -1,4 +1,4 @@
-const clientRoot = (process.env.NODE_ENV !== 'production') ? 'https://moodify.ca' : 'http://localhost:3000';
-const serverRoot = (process.env.NODE_ENV !== 'production') ? 'https://moodify.ca/c' : 'http://localhost:9000';
+const clientRoot = (process.env.NODE_ENV === 'production') ? 'https://moodify.ca' : 'http://localhost:3000';
+const serverRoot = (process.env.NODE_ENV === 'production') ? 'https://moodify.ca/c' : 'http://localhost:9000';
 
 module.exports = { clientRoot, serverRoot };
