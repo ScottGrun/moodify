def069bcc39b7ece39045c2d59bcf44c64b1c534
Tania Nardandrea-Isakovic <rtnrtn@gmail.com>
2020-10-05 05:19:39 +0000

    Export clientRoot and serverRoot constants correctly on server.

--- server/env.js
+++ server/env.js
@@ -1,2 +1,4 @@
-export const clientRoot = (process.env.NODE_ENV !== 'production') ? 'https://moodify.ca' : 'http://localhost:3000';
-export const serverRoot = (process.env.NODE_ENV !== 'production') ? 'https://moodify.ca/c' : 'http://localhost:9000';
+const clientRoot = (process.env.NODE_ENV !== 'production') ? 'https://moodify.ca' : 'http://localhost:3000';
+const serverRoot = (process.env.NODE_ENV !== 'production') ? 'https://moodify.ca/c' : 'http://localhost:9000';
+
+module.exports = { clientRoot, serverRoot };
